e427eaa508ec5fe8e14996dc0f2c2bb6126a00d8
Lekef <wiktorcader@gmail.com>
2022-07-27 21:03:49 +0200

    Pierwsza wersja scatter_squares

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("Dzialaj!")
+from re import A
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=200)
+
+#Zdefiniowanie tytułu wykresu i etykiet osi.
+ax.set_title("Kwadrat liczb", fontsize=24)
+ax.set_xlabel("Wartość", fontsize=14)
+ax.set_ylabel("Kwadraty wartości", fontsize=14)
+
+#Zdefiniowanie wielkości etykiet.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
